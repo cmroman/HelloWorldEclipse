3f7f88041c4ef24fa2d69d102da793f009043ca6
cmroman <cristinamroman@gmail.com>
2017-01-31 14:47:58 -0600

    Changed name to Cristina

--- HelloWorld2/src/HelloWorld.java
+++ HelloWorld2/src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Mutch!");	}
+		System.out.println("Hello Cristina!");	}
 
 }
